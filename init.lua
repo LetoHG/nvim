2672c72e9e17892e0bc784663922bbb4a26d3063
Herwig Letofsky <herwig.m.letofsky@gmail.com>
2024-12-13 15:59:52 +0100

    [style] set rainglow-jewel as main colorscheme

--- init.lua
+++ init.lua
@@ -842,26 +842,37 @@ require('lazy').setup({
     -- vim.cmd.colorscheme 'atlantic-dark'
   },
   { 'neanias/everforest-nvim' },
+  { 'rebelot/kanagawa.nvim' },
   { 'i3d/vim-jimbothemes' },
   { 'rodnaph/vim-color-schemes' },
   { 'realbucksavage/riderdark.vim' },
-  { 'rainglow/vim' },
+  {
+    'rainglow/vim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      -- Load the colorscheme here.
+      vim.cmd.colorscheme 'jewel'
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+    -- priority = 1000, -- Make sure to load this before all the other start plugins.
+    -- init = function()
+    --   -- Load the colorscheme here.
+    --   -- Like many other themes, this one has different styles, and you could load
+    --   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    --   vim.cmd.colorscheme 'tokyonight-night'
 
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
+    --   -- You can configure highlights by doing something like:
+    --   vim.cmd.hi 'Comment gui=none'
+    -- end,
   },
 
   -- Highlight todo, notes, etc in comments
