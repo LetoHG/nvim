397268dd4b8841ffbbd806bc0720a2904d20558c
Herwig Letofsky <herwig.m.letofsky@gmail.com>
2026-01-13 14:11:50 +0100

    [cfg] upgrade leap and fix config

--- lua/custom/plugins/leap.lua
+++ lua/custom/plugins/leap.lua
@@ -1,10 +1,12 @@
 return {
   {
-    'ggandor/leap.nvim',
+    'https://codeberg.org/andyg/leap.nvim',
     config = function()
       local leap = require 'leap'
-      -- leap.add_default_mappings(true)
       leap.opts.case_sensitive = true
+      vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
+      vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
+      vim.keymap.set('n', 'gs', '<Plug>(leap-from-window)')
     end,
     lazy = false,
   },
