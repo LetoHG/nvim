5a39b4b8c922a8ed258543be0090a61999082390
Herwig Letofsky <herwig.m.letofsky@gmail.com>
2026-01-23 12:30:48 +0100

    [cfg] add picker resume map

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -101,22 +101,109 @@ return {
     },
     -- NOTE: Keymaps
     keys = {
-      {'<leader>lg', function() require('snacks').lazygit() end, desc = 'Lazygit',},
-      {'<leader>gl', function() require('snacks').lazygit.log() end, desc = 'Lazygit Logs',},
-      {'<leader>es', function() require('snacks').explorer() end, desc = 'Open Snacks Explorer',},
-      {'<leader>rN', function() require('snacks').rename.rename_file() end, desc = 'Fast Rename Current File',},
-      { '<leader>dB', function() require('snacks').bufdelete() end, desc = 'Delete or Close Buffer  (Confirm)', },
-      -- Snacks Picker 
-      { '<leader>pf', function() require('snacks').picker.files() end, desc = 'Find Files (Snacks Picker)', },
-      { '<leader>pc', function() require('snacks').picker.files { cwd = vim.fn.stdpath 'config' } end, desc = 'Find Config File', },
-      { '<leader>ps', function() require('snacks').picker.grep { layout = 'ivy' } end, desc = 'Grep word', },
-      { '<leader>pws', function() require('snacks').picker.grep_word() end, desc = 'Search Visual selection or Word', mode = { 'n', 'x' }, },
-      { '<leader>pk', function() require('snacks').picker.keymaps { layout = 'ivy' } end, desc = 'Search Keymaps (Snacks Picker)', },
-      -- Git Stuff 
-      { '<leader>gbr', function() require('snacks').picker.git_branches { layout = 'select' } end, desc = 'Pick and Switch Git Branches', },
-      -- Other Utils 
-      { '<leader>pl', function() require('snacks').picker.colorschemes { layout = 'ivy' } end, desc = 'Pick Color Schemes', },
-      {'<leader>vh', function() require('snacks').picker.help() end, desc = 'Help Pages',},
+      {
+        '<leader>lg',
+        function()
+          require('snacks').lazygit()
+        end,
+        desc = 'Lazygit',
+      },
+      {
+        '<leader>gl',
+        function()
+          require('snacks').lazygit.log()
+        end,
+        desc = 'Lazygit Logs',
+      },
+      {
+        '<leader>es',
+        function()
+          require('snacks').explorer()
+        end,
+        desc = 'Open Snacks Explorer',
+      },
+      {
+        '<leader>rN',
+        function()
+          require('snacks').rename.rename_file()
+        end,
+        desc = 'Fast Rename Current File',
+      },
+      {
+        '<leader>dB',
+        function()
+          require('snacks').bufdelete()
+        end,
+        desc = 'Delete or Close Buffer  (Confirm)',
+      },
+      -- Snacks Picker
+      {
+        '<leader>pf',
+        function()
+          require('snacks').picker.files()
+        end,
+        desc = 'Find Files (Snacks Picker)',
+      },
+      {
+        '<leader>pc',
+        function()
+          require('snacks').picker.files { cwd = vim.fn.stdpath 'config' }
+        end,
+        desc = 'Find Config File',
+      },
+      {
+        '<leader>ps',
+        function()
+          require('snacks').picker.grep { layout = 'sidebar' }
+        end,
+        desc = 'Grep word',
+      },
+      {
+        '<leader>pws',
+        function()
+          require('snacks').picker.grep_word { layout = 'sidebar' }
+        end,
+        desc = 'Search Visual selection or Word',
+        mode = { 'n', 'x' },
+      },
+      {
+        '<leader>pk',
+        function()
+          require('snacks').picker.keymaps { layout = 'ivy' }
+        end,
+        desc = 'Search Keymaps (Snacks Picker)',
+      },
+      -- Git Stuff
+      {
+        '<leader>gbr',
+        function()
+          require('snacks').picker.git_branches { layout = 'select' }
+        end,
+        desc = 'Pick and Switch Git Branches',
+      },
+      -- Other Utils
+      {
+        '<leader>pl',
+        function()
+          require('snacks').picker.colorschemes { layout = 'select' }
+        end,
+        desc = 'Pick Color Schemes',
+      },
+      {
+        '<leader>vh',
+        function()
+          require('snacks').picker.help()
+        end,
+        desc = 'Help Pages',
+      },
+      -- Resume picking
+      {
+        '<leader>pr',
+        function()
+          require('snacks').picker.resume()
+        end,
+        desc = 'Resume last picker',
+      },
     },
   },
   -- NOTE: todo comments w/ snacks
@@ -125,8 +212,20 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     optional = true,
     keys = {
-      { '<leader>pt', function() require('snacks').picker.todo_comments() end, desc = 'Todo', },
-      { '<leader>pT', function() require('snacks').picker.todo_comments { keywords = { 'TODO', 'FIX', 'FIXME' } } end, desc = 'Todo/Fix/Fixme', },
+      {
+        '<leader>pt',
+        function()
+          require('snacks').picker.todo_comments()
+        end,
+        desc = 'Todo',
+      },
+      {
+        '<leader>pT',
+        function()
+          require('snacks').picker.todo_comments { keywords = { 'TODO', 'FIX', 'FIXME' } }
+        end,
+        desc = 'Todo/Fix/Fixme',
+      },
     },
   },
 }
