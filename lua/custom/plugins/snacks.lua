8ae1910fa633882ba1054de8b8870bc1cf7c7921
Herwig Letofsky <herwig.m.letofsky@gmail.com>
2026-01-23 12:30:48 +0100

    [cfg] add picker resume map

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -117,6 +117,9 @@ return {
       -- Other Utils 
       { '<leader>pl', function() require('snacks').picker.colorschemes { layout = 'ivy' } end, desc = 'Pick Color Schemes', },
       {'<leader>vh', function() require('snacks').picker.help() end, desc = 'Help Pages',},
+      -- Resume picking
+      { '<leader>pr', function() require('snacks').picker.resume() end, desc = 'Resume last picker',
+      },
     },
   },
   -- NOTE: todo comments w/ snacks
