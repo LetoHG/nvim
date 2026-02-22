8109bb08b2468260aec5667cb932119d56e50fff
Herwig Letofsky <herwig.m.letofsky@gmail.com>
2026-02-22 18:12:50 +0000

    cfg: add copilot plugin

---
+++ lua/custom/plugins/copilot.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      'github/copilot.vim',
+      {
+        'nvim-lua/plenary.nvim',
+        branch = 'master',
+      },
+    },
+    build = 'make tiktoken',
+    opts = {
+      -- See Configuration section for options
+    },
+    -- :CopilotChat <input>?	Open chat with optional input
+    -- :CopilotChatOpen	Open chat window
+    -- :CopilotChatClose	Close chat window
+    -- :CopilotChatToggle	Toggle chat window
+    -- :CopilotChatStop	Stop current output
+    -- :CopilotChatReset	Reset chat window
+    -- :CopilotChatSave <name>?	Save chat history
+    -- :CopilotChatLoad <name>?	Load chat history
+    -- :CopilotChatPrompts	View/select prompt templates
+    -- :CopilotChatModels	View/select available models
+    -- :CopilotChat<PromptName>
+    keys = {
+      { '<leader>ze', '<cmd>CopilotChatExplain<cr>', mode = 'v', desc = 'Copilot: Explain Code' },
+      { '<leader>zr', '<cmd>CopilotChatReview<cr>', mode = 'v', desc = 'Copilot: Review Code' },
+      { '<leader>zf', '<cmd>CopilotChatFix<cr>', mode = 'v', desc = 'Copilot: Fix Code Issues' },
+      { '<leader>zo', '<cmd>CopilotChatOpimize<cr>', mode = 'v', desc = 'Copilot: Opimize Code' },
+      { '<leader>zd', '<cmd>CopilotChatDocs<cr>', mode = 'v', desc = 'Copilot: Generate Docs' },
+      { '<leader>zt', '<cmd>CopilotChatTests<cr>', mode = 'v', desc = 'Copilot: Generate Tests' },
+      { '<leader>zm', '<cmd>CopilotChatCommit<cr>', mode = 'n', desc = 'Copilot: Generate Commit Message' },
+      { '<leader>zs', '<cmd>CopilotChatCommit<cr>', mode = 'v', desc = 'Copilot: Gerenate Commit from Selection' },
+
+      { '<leader>zcc', '<cmd>CopilotChat<cr>', mode = 'n', desc = 'Copilot: Open Chat' },
+      { '<leader>zct', '<cmd>CopilotChatToggle<cr>', mode = 'n', desc = 'Copilot: Toggle Chat' },
+      { '<leader>zcs', '<cmd>CopilotChatStop<cr>', mode = 'n', desc = 'Copilot: Stop Output' },
+      { '<leader>zcr', '<cmd>CopilotChatReset<cr>', mode = 'n', desc = 'Copilot: Reset Chat' },
+      { '<leader>zcS', '<cmd>CopilotChatSave<cr>', mode = 'n', desc = 'Copilot: Save Chat' },
+      { '<leader>zcL', '<cmd>CopilotChatLoad<cr>', mode = 'n', desc = 'Copilot: Load Chat' },
+      { '<leader>zcp', '<cmd>CopilotChatPrompts<cr>', mode = 'n', desc = 'Copilot: Prompts' },
+      { '<leader>zcm', '<cmd>CopilotChatModels<cr>', mode = 'n', desc = 'Copilot: Models' },
+    },
+  },
+}
